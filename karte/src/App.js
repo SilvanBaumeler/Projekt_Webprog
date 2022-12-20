151d44c2cc7e8f2f95d511ed0cc9e1a2f3a2de08
SilvanBaumeler <silvan.baumeler@students.fhnw.ch>
2022-12-20 17:46:04 +0100

    überarbeitung

--- karte/src/App.js
+++ karte/src/App.js
@@ -1,43 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import Button from '@mui/material/Button';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import axios from "axios";
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-
-
-
 import 'leaflet/dist/leaflet.css';
-
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 
 function App() {
   const [data, setData] = useState(null);
-  const [startLat, setStartLat] = useState(0)
-  const [startLon, setStartLon] = useState(0)
-  const [endLat, setEndLat] = useState(0)
-  const [endLon, setEndLon] = useState(0)
+  const [startLat, setStartLat] = useState(1)
+  const [startLon, setStartLon] = useState(1)
+  const [endLat, setEndLat] = useState(1)
+  const [endLon, setEndLon] = useState(1)
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  // const [pkt, setPkt] = useState(100)
+  const geoJsonLayer = useRef(null);
+
 
   useEffect(() => {
     const L = require("leaflet");
     delete L.Icon.Default.prototype._getIconUrl;
     L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-    iconUrl: require("leaflet/dist/images/marker-icon.png"),
-    shadowUrl: require("leaflet/dist/images/marker-shadow.png")//,
-   // window: scrollTo({top: 0, left: 0, behavior: 'smooth'})
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
     });
-    do_download();
     }, []);
 
+  useEffect(() => {
+   if(geoJsonLayer.current) {
+      geoJsonLayer.current.clearLayers().addData(data)
+    }
+  }, [data]);
 
+    function reload() {
+      window.location.reload();
+    }
 
   function do_download() {
-    // TODO: Parametrisieren
     var url = `https://vm1.sourcelab.ch/geodetic/line?startlat=${startLat}&startlng=${startLon}&endlat=${endLat}&endlng=${endLon}&pts=100`;
    
     setLoading(true);
@@ -54,15 +56,16 @@ function App() {
       });
   }
 
+
   return (
     <>
-      <AppBar position="sticky" color="primary">
+       <AppBar position="sticky" color="primary">
             <Toolbar>
                 <Typography variant='h4'>Geodetic Line</Typography>
                 
             </Toolbar>
-            <  iconbutton
-        onClick={() => {
+            <IconButton
+          onClick={() => {
           window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
         }}
         style={{
@@ -78,10 +81,9 @@ function App() {
         }}
       >
         top
-      </iconbutton>
+      </IconButton>
             </AppBar>
             <br/>
-           
 
       <Grid container spacing={2}>
           <Grid container item xs={6} spacing={-8}>
@@ -89,36 +91,34 @@ function App() {
                 <h3>Startpunkt:</h3>
             </Grid>
             <Grid item xs={4}>
-              <TextField type = "number" label="Startpunkt Lat" variant="outlined" onChange={(e) => startLat(e.target.value)} />
+              <TextField type = "number" label="Startpunkt Lat" variant="outlined" onChange={(e) => setStartLat(e.target.value)} />
             </Grid>
             <Grid item xs={4}>
-              <TextField type = "number" label="Startpunkt Lon" variant="outlined" onChange={(e) => startLon(e.target.value)}/>
+              <TextField type = "number" label="Startpunkt Lon" variant="outlined" onChange={(e) => setStartLon(e.target.value)} />
             </Grid>
           </Grid>
-
 
           <Grid container item xs={6} spacing={-8}>
             <Grid item xs={2}>
                 <h3>Endpunkt:</h3>
             </Grid>
             <Grid item xs={4}>
-              <TextField type = "number" label="Endpunkt Lat" variant="outlined" onChange={(e) => endLat(e.target.value)}/>
+              <TextField type = "number" label="Endpunkt Lat" variant="outlined" onChange={(e) => setEndLat(e.target.value)}/>
             </Grid>
             <Grid item xs={4}>
-              <TextField type = "number" label="Endpunkt Lon" variant="outlined" onChange={(e) => endLon(e.target.value)}/>
+              <TextField type = "number" label="Endpunkt Lon" variant="outlined" onChange={(e) => setEndLon(e.target.value)}/>
             </Grid>
-            </Grid>
+          </Grid>
 
 
-          <Grid item xs={0}>
-          <Button variant="contained" onClick={() => { do_download() }}>
-          Berechnen
-        </Button><p/>
-        
-        
+          <Grid item xs={12}>
+            <Button variant="contained" onClick = { () => {do_download()}}>
+            Convert
+            </Button><p/>
+            <Button variant="contained" onClick={() => { reload() }}>Reset</Button>
           </Grid>
         </Grid>
-   
+
       {loading && <>
                      <div>API Aufruf, bitte warten!</div><br/>
                   </>
@@ -129,19 +129,17 @@ function App() {
                   </>}
 
       {data &&  <>
-                  <MapContainer center={[47.5349, 7.6416]} zoom={2} scrollWheelZoom={true}
-                    style={{ height: "600px", width: "100%" }} >
-                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+          <MapContainer center={[47.5349, 7.6416]} zoom={2} scrollWheelZoom={true}
+          style={{ height: "600px", width: "100%" }} >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
                   
-                  <GeoJSON data={data} style={{ weight: 8, opacity: '50%', color: 'blue'}}/>
+           <GeoJSON data={data} style={{ weight: 8, opacity: '50%', color: 'blue'}}/>
 
-                  </MapContainer>
+          </MapContainer>
                 </>}
-       
+  
       </>
-
-      
   );
 }
 
